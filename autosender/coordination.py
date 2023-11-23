1dc266a0df84bef782851c0c27281c8e6b68c2d9
eloise-chou <sherry880116@gmail.com>
2023-11-23 10:41:47 +0800

    add picture_coordinate/find_message_coordinate/disable_voucher_coordinate.

--- autosender/coordination.py
+++ autosender/coordination.py
@@ -1,19 +1,44 @@
 line_coordinate = {
-    'search'    : (160, 125),
-    'type'      : (1000, 860),
+    'search'      : (160, 125),
+    'type'        : (1000, 860),
     'first_result':(500, 230)
 }
 
 skype_coordinate = {
-    'search'    : (100, 150),
-    'type'      : (700, 935),
+    'search'      : (100, 150),
+    'type'        : (700, 935),
     'first_result': (200, 450)
 }
 
-shopee_coordinate = {
-    'search'    : (392, 392),
-    'type'      : (524, 945),
-    'send'      : (1641, 839),
+picture_coordinate = {
+    'search'      : (160, 125),
+    'type'        : (1000, 860),
+    'first_result': (500, 230),
+    'add_file'    : (596, 992),
+    'file'        : (400, 260),
+    'open_file'   : (700, 680),
+}
+
+find_message_coordinate = {
+    'search'        : (160, 125),
+    'type'          : (1000, 860),
+    'first_result'  : (500, 230),
+    'message_search': (1768, 120),
+    'type-in'       : (700, 200),
+    'search_type'   : (700, 250),
+    'pos_click'     : (1475, 600),
+    'reply'         : (1550, 625),
+} 
+
+disable_voucher_coordinate = {
+    'search'                : (600,400),
+    'type'                  : (1000, 860),
+    'first_result'          : (500, 230),
+    'search_button'         : (1050, 630),
+    'view_button'           : (1725, 785),
+    'disable_button'        : (1500, 280),
+    'disable_comfirm_button': (1650, 400),
+    'return_button'         : (60, 300),
 }
 
 test_coordinate = {
@@ -23,8 +48,10 @@ test_coordinate = {
 }
 
 coordinate_info = {
-    'line'  : line_coordinate,
-    'skype' : skype_coordinate,
-    'shopee': shopee_coordinate,
-    'test'  : test_coordinate,
+    'line'           : line_coordinate,
+    'skype'          : skype_coordinate,
+    'with_picture'   : picture_coordinate,
+    'search_message' : find_message_coordinate,
+    'disable_voucher': disable_voucher_coordinate,
+    'test'           : test_coordinate,
 }
